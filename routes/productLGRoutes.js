2e044a1ae16e83db8804db8325943e58126ff129
Rodritdc <129792625+Rodritdc@users.noreply.github.com>
2023-04-28 01:13:42 -0400

    Update productLGRoutes.js

--- routes/productLGRoutes.js
+++ routes/productLGRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {
- createProductLGController,updateProductLGController,updateOfferLGController,deleteOfferLGCOntroller ,ProductLGControlller,singleProductLGController,deleteProductLGCOntroller
+  createProductLGController,updateProductLGController,updateOfferLGController,deleteOfferLGCOntroller ,ProductLGControlller,singleProductLGController,deleteProductLGCOntroller
 } from "../controllers/productLGController.js";
 import formidable from "express-formidable";
 
