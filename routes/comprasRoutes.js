e1474f02490958261098218728777ecddbacb01c
marinsebastian <marin.sebastian.143@gmail.com>
2023-04-28 17:50:13 -0400

    obtener datos para tabla HU:stock

--- routes/comprasRoutes.js
+++ routes/comprasRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { createCompraController } from "../controllers/comprasController.js";
+import { createCompraController,comprasGetControlller } from "../controllers/comprasController.js";
 
 const router = express.Router()
 
 //routes 
 //create category
 router.post('/create-compra',createCompraController)
+//getAll compras
+router.get("/get-compras", comprasGetControlller);
 
 export default router
 
