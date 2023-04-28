92c860c20b2e363c50c0aecad0dfd97a53527039
Rodritdc <129792625+Rodritdc@users.noreply.github.com>
2023-04-28 00:56:28 -0400

    Update productLGRoutes.js

--- routes/productLGRoutes.js
+++ routes/productLGRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import {
-  createProductLGController,updateProductLGController, ProductLGControlller,singleProductLGController,deleteProductLGCOntroller, updateOfferLGController, deleteOfferLGCOntroller
-} from "../controllers/productLGController.js";
+  createProductLGController,updateProductLGController, ProductLGControlller,singleProductLGController,deleteProductLGCOntroller
+  } from "../controllers/productLGController.js";
 import formidable from "express-formidable";
 
 const router = express.Router();
