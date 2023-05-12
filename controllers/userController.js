0945a53721779fd0388d4286765c260fe6349ab8
marinsebastian <104607937+marinsebastian@users.noreply.github.com>
2023-05-12 16:34:21 -0400

    Back de registrar usuario (#21)

---
+++ controllers/userController.js
@@ -0,0 +1,54 @@
+import userModel from "../models/userModel.js";
+import { comparePassword, hashPassword} from "../helpers/authHelpers.js";
+import JWT from "jsonwebtoken";
+
+export const registerUserController = async (req, res) => {
+  try {
+    const { email, name, lastname, phone, password,} = req.body;
+    //validations
+    if (!name) {
+      return res.send({ error: "Name is Required" });
+    }
+    if (!email) {
+      return res.send({ message: "Email is Required" });
+    }
+    if (!password) {
+      return res.send({ message: "Password is Required" });
+    }
+    if (!phone) {
+      return res.send({ message: "Phone no is Required" });
+    }
+    //check user
+    const exisitingUser = await userModel.findOne({ email });
+    //exisiting user
+    if (exisitingUser) {
+      return res.status(200).send({
+        success: false,
+        message: "Already Register please login",
+      });
+    }
+    //register user
+    const hashedPassword = await hashPassword(password);
+    //save
+    const user = await new userModel({
+      email,
+      name,
+      lastname,
+      phone,
+      password: hashedPassword,
+    }).save();
+
+    res.status(201).send({
+      success: true,
+      message: "User Register Successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Errro in Registeration",
+      error,
+    });
+  }
+};
