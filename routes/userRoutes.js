6ac6750f91ececd585d9034d379aaed94a312352
unknown <reinaga63.jr@gmail.com>
2023-05-21 23:41:08 -0400

    rutas controlador usuario

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,11 @@ const router = express.Router()
 //routes 
 //register user
 router.post('/register-user',registerUserController)
-
+router.get("/get-User/:email", singleUserLGController);
+//update
+router.put(
+    "/update-User",updateUserController
+  );
 //login user
   
 export default router
